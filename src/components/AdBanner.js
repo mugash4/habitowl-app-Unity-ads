32791e51220fd00ca3ed42d1ffc30907c4d95226
mugash4 <mugash4@gmail.com>
2025-10-18 15:48:52 +0300

    Added missing files

---
+++ src/components/AdBanner.js
@@ -0,0 +1,12 @@
+/**
+ * AdBanner Component - Wrapper for Unity Banner Ads
+ * This file acts as an alias to maintain backward compatibility
+ */
+
+import UnityBannerAd from './UnityBannerAd';
+
+// Export UnityBannerAd as default
+export default UnityBannerAd;
+
+// Also export as named export if needed
+export const AdBanner = UnityBannerAd;
